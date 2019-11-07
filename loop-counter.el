fd0211f4b5783d9b793873b7898feac954662be5
pcolsen <pcolsen@gmail.com>
2019-11-07 12:33:12 -0500

    Thie is th e initial commit for loop-utilities.el and loop-counter.el.
    loop-utilities.el is a working file with closures and wrapping functions
    for counting things and stacking things.  Future commits probably will
    contain new functions and the count and stack functions unchanged
    
    loop-counter is a non-working file containing a loop counting function
    that must be rewritten.  Future commits will be very different.

---
+++ loop-counter.el
@@ -0,0 +1,66 @@
+;;;-*- mode: Emacs-lisp; lexical-binding: t ; -*-
+
+(defvar input-buffer nil)
+(defvar output-buffer nil)
+
+(defun say-thing (thing)
+  "This function takes a 'thing', formats it into 
+   a string, then both writes it as a message and
+   inserts it into the output buffer followed by '\n'."
+  (let ((string (format "%s" thing))) 
+    (message string)
+    (with-current-buffer output-buffer
+      (insert (concat ";;;" string "\n")))))
+		 
+;;; I HAVE TO FINISH REFACTORING THIS
+(defun count-loops-directory (target-directory-name)
+  ;; Is the directory accessible and writable. Bail if not
+  (let ((target-directory-true-name (truename target-directory-name)))
+    (if (file-accessible-directory-p target-directory-true-name
+      ;; Directory is writable, so we process it.
+      (let (;; Need canonical directory name to write output file
+	    (output-directory-name 
+	     (file-name-as-directory target-directory-name))
+	    ;; True name to label output file
+	    (directory-true-name (truename target-directory-name)))
+	(let (;; All output will be written to a buffer for one file.
+	      (out-file-name  (concat output-directory-name
+				      target-directory-name "-loop-counts.txt"))
+	      ;; Header for output file to identify directory
+	      (header-string
+	       (concat ";;; loop counts for directory: " directory-true-name)))
+	  ;; -----------------------------------------------
+	  ;; "output-buffer" is global
+	  (setq output-buffer (get-buffer-create out-file-name))
+	  (if output-buffer ;; No point if we can't create the buffer
+	      ;; Buffer was created
+	      (with-current-buffer
+		  (say-thing header-string)
+		(seve-buffer output-buffer) ; Save it now if it crashes later.
+		;; --------------------------------------
+		;; Here is where all the work will be done.
+		(process-directory target-directory-name)
+		;; The rest is just testing conditions and comments
+		;; --------------------------------------
+		(say-thing (concat ";;; Processing: "
+				   directory-true-name "finished"))
+		(save-buffer output-buffer)) ; Make sure buffer is saved.
+	    ;; If get here the Buffer wasn't created
+	    ;; so we can't leave a message in it.
+	    (message (concat "Error: Could not create output buffer for: "
+			     outfile-name)))))
+	;; We come here if the directory wasn't writable
+	(progn
+	  (message (concat "Directory '" (truename target-directory-name)
+			   "' could not be "
+		   "opened or is not writable")))))
+  nil)
+
+(defun test-setup ()
+  (defvar output-buffer '())
+  (defvar test-dir-name "~/temp/loop-counter-test-dir")
+  (if (not ((file-accessible-directory-p test-dir-name)))
+      (make-directory test-dir-name))
+      
+	
+  
---
+++ loop-utilities.el
@@ -0,0 +1,85 @@
+;;;-*- mode: Emacs-lisp; lexical-binding: t ; -*-
+
+(setq lexical-binding t)
+
+
+;;; These can't be used in a constructor function because then
+;;; count-list and stack-list will be limited to the scope of the
+;;; constructor
+
+
+(let ((count-list nil))
+  "This let construction makes a closure used for counting things.
+   the function counts things using an association list.
+   You can think of thie as an array indexed by the th8ngs 
+   being couinted.  The first element of each of the association
+   cells is one of the things to be counted (analogous to an 
+   array index).  The second is a list containing a count of 
+   those things.  This function always returns the current list
+   of counts.  Calling this function with a non-nil value of thing
+   either increments the count, or, if thing hasn't been seen 
+   before, it creates a count for thing initializing it to 1.
+   Calling it with a nil argument returns the
+   count-list without changing any of the counts."
+  (setq counter-closure
+	(lambda (&optional thing) ;; thing is what is to be counted
+	  ;; (mmp (list "Initial argument: thing: " thing))
+	  (cond
+	   ((and thing count-list) ;; Here we have a thing to be
+	    ;; counted and a list of counts of things.  We're going
+	    ;; to update the count of thing,  We'll then return
+	    ;; count-list.
+	    (let ((count-pair (assoc thing count-list)))
+	      (if count-pair
+		  (let ((current-count (car (cdr count-pair))))
+		    ;; (mmp count-pair)
+		    ;; (mmp current-count)
+		    (setcdr count-pair (list (+ 1 current-count))))
+		(setq count-list (cons (list thing 1) count-list)))
+	      count-list))  ;; We'll return the count list
+	   (thing
+	    ;; This will be the first thing to be counted, so
+	    ;; we have to create the count-list from scrath,
+	    ;; then return it.
+	    ;; (mmp (list "Entering initial count: " thing))
+	    (setq count-list (list (list thing 1)))) 
+	   (t
+	    ;; We've been called with a nil argument, so we'll
+	    ;; just return the count-list.
+	    count-list)))))
+
+(provide 'count-closure)
+;;; Definition of count-closure ends here
+
+(defun count-thing (thing)
+  (funcall counter-closure thing))
+
+(provide 'count-thing)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defvar stack)
+
+(let ((stack-list nil))
+  "This let construction is a stack using the list stack-list
+   as the stack structure itself.  If it is called with a non-nil
+   argument, that argument is pushed onto the stack and the
+   stacklist is returned.  If the argument is nil, then the stack
+   is popped and the popped element is returned."
+  (setq stack-closure
+	(lambda (&optional thing) ;; thing is what is to be counted
+	  ;; (mmp (list "Initial argument: thing: " thing))
+	    (if thing
+		(push thing stack-list)
+	      (pop stack-list)))))
+
+(provide 'stack-closure)
+;;; Definition of stack-closure ends here
+
+(defun stack-thing (thing)
+  (funcall stack-closure thing))
+
+(provide 'stack-thing)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
